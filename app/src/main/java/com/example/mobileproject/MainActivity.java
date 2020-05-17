6af1d7d4e877eecd8f8bab59e6920519fd7d8e54
Antoine <49623494+antoinemairet@users.noreply.github.com>
2020-05-17 11:33:46 +0200

    Affichage d'une liste basique

--- app/src/main/java/com/example/mobileproject/MainActivity.java
+++ app/src/main/java/com/example/mobileproject/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.mobileproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private ListAdapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        List<String> input = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            input.add("Test" + i);
+        }// define an adapter
+        mAdapter = new ListAdapter(input);
+        recyclerView.setAdapter(mAdapter);
     }
 }
